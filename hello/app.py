73c69c5fe8e0c7d0bed5740bddc382afc9eea788
jason <1875104537@qq.com>
2020-07-30 17:59:47 +0800

    initialize project

---
+++ hello/app.py
@@ -0,0 +1,13 @@
+#-*- coding: utf-8 -*-
+from flask import Flask
+app = Flask(__name__)
+print(app.name)
+
+@app.route('/')
+def index():
+    return '<h1>Hello World!<h1>'
+
+@app.route('/greet', defaults={'name':'Programmer'})
+@app.route('/greet/<name>')
+def greet(name):
+    return '<h1>Hello, %s!' % name
